5c12df9d00c4a78a56f54a3eff7731be3f7d615b
JSK <spdl9793@naver.com>
2018-09-19 15:24:40 +0900

    store관련매퍼 만듬

---
+++ cafeKiosk_v2/src/org/zerock/dao/StoreDAO.java
@@ -0,0 +1,18 @@
+package org.zerock.dao;
+
+import org.apache.ibatis.session.SqlSession;
+import org.zerock.domain.OwnerVO;
+
+public class StoreDAO {
+	private String prefix ="org.zerock.mapper.";
+	
+	public void getOwner(OwnerVO vo) {
+		try(SqlSession session = MyBatisLoader.sqlSessionFactory.openSession(true)){
+			
+			session.insert(prefix + ".getOwner",vo);
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}

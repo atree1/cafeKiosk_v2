b9edbd40ba64ea8b15d2bc46ee97732a88c1ad6b
BIT03-04 <BIT03-04@BIT03-04>
2018-09-19 19:58:17 +0900

    디테일 수정

---
+++ cafeKiosk_v2/src/org/zerock/dao/OrderDetailDAO.java
@@ -0,0 +1,25 @@
+package org.zerock.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.zerock.domain.OrderDetailVO;
+import org.zerock.domain.OwnerVO;
+
+public class OrderDetailDAO {
+
+	private final String prefix = "org.zerock.mapper.OrderDetailMapper";
+
+	public List<OrderDetailVO> getDetail(int ono) {
+
+		try (SqlSession session = MyBatisLoader.sqlSessionFactory.openSession(true)) {
+				
+			List<OrderDetailVO> list = session.selectList(prefix + ".getDetail",ono);
+			return list;
+		} catch (Exception e) {
+			e.getMessage();
+		}
+		return null;
+	}
+}

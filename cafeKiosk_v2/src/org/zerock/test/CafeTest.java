aae0bb317b2ba16f1840952215e0f02fe0a65a3a
5CLASS-184 <5CLASS-184@BIT03-14>
2018-09-19 17:40:52 +0900

    abstract

--- cafeKiosk_v2/src/org/zerock/test/CafeTest.java
+++ cafeKiosk_v2/src/org/zerock/test/CafeTest.java
@@ -1,11 +1,19 @@
 package org.zerock.test;
 
 import org.junit.Test;
+import org.zerock.dao.OrderDAO;
 import org.zerock.dao.StoreDAO;
 
 public class CafeTest {
+	OrderDAO dao=new OrderDAO();
+	StoreDAO sdao=new StoreDAO();
 	@Test
+	public void getOrder() {
+		
+		dao.updateState(23);
+	}
+	
 	public void getStore() {
-		System.out.println(new StoreDAO().getStore(1));
+		System.out.println(sdao.getStore(1));
 	}
 }
